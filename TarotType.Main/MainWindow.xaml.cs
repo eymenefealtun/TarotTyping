b46e01205ca1ca475e5a4c5caff4b8d970500f0c
eymenefealtun <eymenefealtun@hotmail.com>
2023-07-30 20:01:52 +0300

    feat: functionality of coloring words added

--- TarotType.Main/MainWindow.xaml.cs
+++ TarotType.Main/MainWindow.xaml.cs
@@ -29,13 +29,22 @@ namespace TarotType.Main
 
         string _targetText;
         string _currentTextOfTextBox;
+        bool _isTextBoxChangedCanFire;
         private void tboxWrite_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+            if (!_isTextBoxChangedCanFire || tboxWrite.Text == " ")
+            {
+                tboxWrite.Text = String.Empty;
+                return;
+            }
+
+
+
+            //MessageBox.Show("change");
 
             _currentTextOfTextBox = tboxWrite.Text;
-            if (_currentTextOfTextBox.Substring(0, _currentTextOfTextBox.Length) == _currentTextOfTextBox + " ")
-                return;
+
 
             _targetText = _words1[_currentWord1Index].Content.ToString();
 
@@ -71,8 +80,22 @@ namespace TarotType.Main
 
         private void tboxWrite_PreviewKeyDown(object sender, KeyEventArgs e)
         {
+
+
             if (e.Key == Key.Space && tboxWrite.Text != string.Empty)
             {
+                int lastIndex = _words1.Count;
+                if (_currentWord1Index == lastIndex - 1)
+                {
+                    _currentWord1Index = 0;
+
+                    GetAnotherStack(_words2);
+
+                    return;
+                }
+                //MessageBox.Show("previewKeyDown");
+
+                _isTextBoxChangedCanFire = false;
 
                 if (IsWordDoneTrue(_currentTextOfTextBox, _targetText))
                     TextDoneTrue(_words1[_currentWord1Index], _words1[_currentWord1Index + 1]);
@@ -80,34 +103,35 @@ namespace TarotType.Main
                 else
                     TextDoneWrong(_words1[_currentWord1Index], _words1[_currentWord1Index + 1]);
             }
+            else
+                _isTextBoxChangedCanFire = true;
         }
 
         private void TextDoneTrue(Label lbl, Label nextLabel)
         {
+            _numberOfTrueWords++;
+
             lbl.Background = Brushes.Transparent;
             lbl.Foreground = Brushes.Green;
-
-
-
             nextLabel.Background = Brushes.LightGray;
-            _numberOfTrueWords++;
-            tboxWrite.Text = string.Empty;
+
+            //tboxWrite.Text = null;
 
             _currentWord1Index++;
-
         }
 
         private void TextDoneWrong(Label lbl, Label nextLabel)
         {
+            _numberOfWrongWords++;
 
             lbl.Background = Brushes.Transparent;
             lbl.Foreground = Brushes.Red;
-
-
-
             nextLabel.Background = Brushes.LightGray;
-            _numberOfWrongWords++;
-            tboxWrite.Text = string.Empty;
+
+
+
+            //tboxWrite.Text = null;
+
             _currentWord1Index++;
         }
 
@@ -118,55 +142,84 @@ namespace TarotType.Main
 
         private void CurrentTextTrue(Label lbl)
         {
-            lbl.Background = Brushes.LightGray; //sorun
+            lbl.Background = Brushes.LightGray;
         }
 
 
         private void RefreshGame()
         {
-            RefreshStack(stckPanel1, _words1);
-            RefreshStack(stckPanel2, _words2);
+
             _numberOfTrueWords = 0;
             _numberOfWrongWords = 0;
+            _isTextBoxChangedCanFire = true;
+            _currentWord1Index = 0;
+            _targetText = string.Empty;
+            _currentTextOfTextBox = string.Empty;
+            tboxWrite.Text = String.Empty;
+            RefreshStack(stckPanel1, _words1);
+            RefreshStack(stckPanel2, _words2);
+
         }
 
+        private void GetAnotherStack(List<Label> words2)
+        {
+            stckPanel1.Children.Clear();
+            stckPanel2.Children.Clear();
+            _words1 = new List<Label>();
+
+            for (int i = 0; i < words2.Count; i++)
+            {
+                stckPanel1.Children.Insert(i, words2[i]);
+                _words1.Add(_words2[i]);
+            }
+
+
+
+
+
+            tboxWrite.Text = String.Empty;
+            RefreshStack(stckPanel2, _words2);
+        }
 
         private void RefreshStack(StackPanel panel, List<Label> labels)
         {
             panel.Children.Clear();
             labels.Clear();
 
-            string[] array = WordManager.GetRandomWord(204);
+            string[] array = WordManager.GetRandomWord(20);
 
             int currentLength = 0;
+
             for (int i = 0; i < array.Length; i++)
             {
-                currentLength += array[i].Length;
-                if (currentLength < 62)
+
+                Label lbl = new Label();
+                if (i == 0 && labels == _words1)
                 {
-                    Label lbl = new Label();
-                    if (i == 0 && labels == _words1)
-                    {
-                        lbl.Content = array[i];
-                        lbl.Background = Brushes.LightGray;
-                        lbl.Style = FindResource("MainTextBlockTheme") as Style;
+                    lbl.Content = array[i];
+                    lbl.Background = Brushes.LightGray;
+                    lbl.Style = FindResource("MainTextBlockTheme") as Style;
 
-                    }
-                    else
-                    {
-                        lbl.Content = array[i];
-                        lbl.Background = Brushes.Transparent;
-                        lbl.Style = FindResource("MainTextBlockTheme") as Style;
-                    }
-
-
-
-                    labels.Add(lbl);
-
-                    panel.Children.Insert(i, lbl);
                 }
                 else
+                {
+                    lbl.Content = array[i];
+                    lbl.Background = Brushes.Transparent;
+                    lbl.Style = (Style)FindResource("MainTextBlockTheme");
+                }
+
+
+
+                lbl.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+                lbl.Arrange(new Rect(lbl.DesiredSize));
+                currentLength += (Convert.ToInt32(lbl.ActualWidth) + 10); //this ten comes from margin of stackPanel
+
+                if (currentLength > stckPanel1.Width)
                     break;
+
+                labels.Add(lbl);
+                panel.Children.Insert(i, lbl);
+
 
             }
         }
