bb89a5a03792a4bcdd8d79311286d4eb98f4d03b
eymenefealtun <eymenefealtun@hotmail.com>
2023-08-28 17:32:11 +0300

    refactor: Pictures to Resources

--- Utilities/Words/Language.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Utilities.Words
-{
-    public abstract class Language
-    {
-        public abstract string Path();
-        public abstract SourceManager.flowDirections FlowDirection();
-    }
-}
